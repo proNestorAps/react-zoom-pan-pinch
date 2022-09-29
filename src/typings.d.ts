cbc869158d087119d5fba5f4e926f983b3ff4013
Jan Aagaard <jan@aagaard.net>
2022-09-29 21:33:12 +0200

    Replace StatelessComponent with FunctionComponent

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -4,4 +4,4 @@ declare module "*.css" {
   export default content;
 }
 
-type SvgrComponent = React.StatelessComponent<React.SVGAttributes<SVGElement>>;
+type SvgrComponent = React.FunctionComponent<React.SVGAttributes<SVGElement>>;
